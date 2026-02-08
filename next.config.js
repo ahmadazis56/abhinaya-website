ddedea9193d799c8bfbc9c9ee38efda265daacb5
Ahmad Azis Supiadi <lokazis12@gmail.com>
2026-02-09 06:49:48 +0800

    Update Next.js config for production deployment

--- next.config.js
+++ next.config.js
@@ -16,6 +16,10 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['better-sqlite3'],
+  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
